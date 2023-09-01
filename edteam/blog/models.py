3512fdf9accebe36a6bc93bb94f9c348ce03a8da
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-09-01 12:42:20 -0600

    4.2: Creación de modelos

--- edteam/blog/models.py
+++ edteam/blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Articulo(models.Model):
+    titulo = models.CharField(max_length=200)
+    imagen = models.CharField(max_length=255)
+    autor = models.CharField(max_length=100)
