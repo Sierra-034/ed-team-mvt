a2332ad1d9bc5a54de60a0d9e42e18ab14b18fe1
Samuel Gómez Balderas <samuel.gomez.balderas@gmail.com>
2023-09-01 13:16:38 -0600

    4.4: Ejercicios con el shell de python

--- edteam/blog/models.py
+++ edteam/blog/models.py
@@ -5,3 +5,6 @@ class Articulo(models.Model):
     titulo = models.CharField(max_length=200)
     imagen = models.CharField(max_length=255)
     autor = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.titulo
